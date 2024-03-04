c7c803d087571f596eb09d1e0382a06f8c64648e
Mathias <math649ivigtig@gmail.com>
2024-03-04 20:31:49 +0100

    Lidt småændringer til KS

---
+++ src/main/java/com/example/frisoersalonprojekt/Controller/KundeSide.java
@@ -0,0 +1,33 @@
+package com.example.frisoersalonprojekt.Controller;
+
+import com.example.frisoersalonprojekt.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class KundeSide {
+
+    @FXML
+    private Button LogUdBtn;
+    @FXML
+    private Button bestilTidBtn;
+
+    @FXML
+    private void LogUd(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("Startside.fxml");
+        System.out.println("Du er nu logget af");
+
+    }
+    @FXML
+    private void SkiftTilTidsbestilling(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("Tidsbestilling.fxml");
+
+    }
+
+
+
+}
