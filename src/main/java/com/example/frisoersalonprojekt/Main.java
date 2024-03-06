195e8f98b9abdf20c60b05352205a27d2df39973
Lauge Maar <lam002@edu.zealand.dk>
2024-03-06 11:04:24 +0100

    hej

--- src/main/java/com/example/frisoersalonprojekt/Main.java
+++ src/main/java/com/example/frisoersalonprojekt/Main.java
@@ -1,11 +1,14 @@
 package com.example.frisoersalonprojekt;
 
+import com.example.frisoersalonprojekt.Klasser.DbSql;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
+
 public class Main extends Application {
 
     private static Stage stg;
@@ -18,12 +21,37 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setTitle("Monikas Frisørsalon");
 
-        // Here you can directly set the width and height of the stage
-        primaryStage.setWidth(1200); // Set the width of the stage
-        primaryStage.setHeight(800); // Set the height of the stage
+        // Set the width and height of the stage
+        primaryStage.setWidth(1200);
+        primaryStage.setHeight(800);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // Initialiser DbSql og opdater tidsbestillinger til 'Afholdt'
+        opdaterTidsbestillinger();
+        rydOpIDatabase();
+    }
+
+    private void opdaterTidsbestillinger() {
+        try {
+            // Antag at du har en constructor i DbSql der ikke kræver argumenter
+            DbSql dbSql = new DbSql();
+            dbSql.opdaterTidsbestillingerTilAfholdt();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Fejl ved opdatering af tidsbestillinger: " + e.getMessage());
+        }
+    }
+    private void rydOpIDatabase() {
+        try {
+            DbSql dbSql = new DbSql();
+            dbSql.opdaterTidsbestillingerTilAfholdt();
+            dbSql.sletGamleTidsbestillinger(); // Sletter gamle tidsbestillinger
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Fejl ved rydning af database: " + e.getMessage());
+        }
     }
 
     public void changeScene(String fxml) throws IOException {
@@ -32,6 +60,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
